e71e3beef09a6b0efc64e87eff7f3a1982cc78a2
jayam3181 <kumarjayam07@gmail.com>
2022-02-27 18:02:14 +0530

    CSS  minor changes

--- js/client.js
+++ js/client.js
@@ -51,7 +51,7 @@ socket.on('user-joined', name => {
     append(`${name} joined the chat`, 'right')
 })
 socket.on('receive', data => {
-    append(`${data.name}:${data.message}`, 'left')
+    append(`${data.name}: ${data.message}`, 'left')
 })
 socket.on('left', name => {
     append(`${name} left the chat`, 'left')
